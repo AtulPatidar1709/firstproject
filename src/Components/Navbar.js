2d25d2704d83dad759875c33d2e6d8c007de4db6
Atul Patidar <Atulpatidar1709@gmail.com>
2022-07-28 23:39:56 +0530

    Props

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default function Navbar(props) {
    return (
@@ -54,3 +55,10 @@ export default function Navbar(props) {
       </nav>
    );
 }
+
+Navbar.propTypes = { title: PropTypes.string, aboutText: PropTypes.string };
+
+Navbar.defaultProps = {
+   title: "Palce Title Here",
+   aboutText: "About Here",
+};
